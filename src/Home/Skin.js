a059391b727f65c90e09b4e6dc3a531143d6e0dd
Diego Oliveira <diegoju95@hotmail.com>
2022-11-05 18:15:46 -0300

    new

--- src/Home/Skin.js
+++ src/Home/Skin.js
@@ -1,74 +1,107 @@
 import React from 'react'
 import { SkinCss } from './Styled/SkinCss'
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 import Html from '../img/htmlcss.png'
 import Js from '../img/js.png'
 import Sty from '../img/style.png'
 import Git from '../img/git.png'
+import Node from '../img/node.png'
 
 
 const Skin = () => {
   const Tecnologias = '< /tecnologias >'
+  let config = {
+    dot: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    cssEase: "linear"
+  }
   return (<SkinCss><div className='corpo'>
     <h1 className='title'>{Tecnologias}</h1>
-
-    <div className='grid'>
-       <div className='card'>
-       <div className='fotoCard'>
-       </div><img alt='git' src={Html}></img>
-       <div className='texto'>
-        <h3>Html/CSS</h3>
-        <p>Conhecimento em linguagem de Marcação de Hipertexto, inputs, formulários, lista, tabelas entre outras
+     <Slider  {...config}>
+<div className='card-corpo'>
+  <div className='card'>
+    <div className='card-img'>
+      <img alt='dev' src={Html}></img>
+      <div className='desc'>
+      <h2>Html/css</h2>
+      <p>Conhecimento em linguagem de Marcação de Hipertexto, inputs, formulários, lista, tabelas entre outras
           . Conhecimento em css para estilização do html e manter ele responsivo para os demais aparelhos, 
           ajudando a melhorar interação com o usuário, através de slides, animações, cards
-           e entre outros elemento de uma pagina Web.</p>
-       </div>
-       </div>
-
-       <div className='card'>
-       <div className='fotoCard'>
-       </div><img alt='git' src={Js}></img>
-       <div className='texto'>
-       <h3>Javascrip/ReactJs</h3>
-       <p>
+           e entre outros.</p>
+     </div>
+    </div>
+  </div>
+</div>
+<div className='card-corpo'>
+  <div className='card'>
+    <div className='card-img'>
+      <img alt='dev' src={Js}></img>
+      <div className='desc'>
+      <h2>Javascrip/ReactJs</h2>
+      <p>
        Conhecimento em JS com variavel, funções, map, números e operadores, if e else, lop, array, await,
         filter, manipulação de inputs e botão, e entre outro. O react tenho experiência em meus 
         projetos com hook, react router, uuid, props, axios para manipulação de Json com métodos
-         http e styled-components.
+         http.
        </p>
-       </div>
-       </div>
-
-       <div className='card'>
-       <div className='fotoCard'>
-       </div><img alt='git' src={Sty}></img>
-       <div className='texto'>
-       <h3>Bootstrap/Styled-components</h3>
-       <p>
+     </div>
+    </div>
+  </div>
+</div>
+<div className='card-corpo'>
+  <div className='card'>
+    <div className='card-img'>
+      <img alt='dev' src={Sty}></img>
+      <div className='desc'>
+      <h2>Bootstrap/Styled-components</h2>
+      <p>
        Conhecimento em Bootstrap, React-Bootstrap e styled-components 
        para estilização de paginas e aplicações web através do html e do ReactJs.
        </p>
-       </div>
-       </div>
-
-       <div className='card'>
-       <div className='fotoCard'>
-       </div><img alt='git' src={Git}></img>
-       <div className='texto'>
-       <h3>Git/GitHub</h3>
-       <p>
+     </div>
+    </div>
+  </div>
+</div>
+<div className='card-corpo'>
+  <div className='card'>
+    <div className='card-img'>
+      <img alt='dev' src={Git}></img>
+      <div className='desc'>
+      <h2>Git/GitHub</h2>
+      <p>
        Conhecimento em git para interagir com a ferramenta github através de comandos como,
         git commit, init, git clone, branch, git add, push, origin, status e entre comandos. 
         Conhecimento em github para deploy, github pages e domínios personalizados. 
        </p>
-       </div>
-       </div>
-
+     </div>
     </div>
-    <div className='bonus'>
-      <p>💡 Conhecimento com ferramenta figma, <br />
-      &nbsp; &nbsp;
-       NodeJs basico e estudando TypeScript.</p>
-    </div></div>
+  </div>
+</div>
+<div className='card-corpo'>
+  <div className='card'>
+    <div className='card-img'>
+      <img alt='dev' src={Node}></img>
+      <div className='desc'>
+      <h2>NodeJs/MariaDb</h2>
+      <p>
+       Basico em nodeJs express para criacao de API e mariaBd para
+       armazenamentos de dados
+       </p>
+     </div>
+    </div>
+  </div>
+</div>
+     </Slider>
+    <div className='detalhes'>
+    🎓 Cursando tecnólogo em Desenvolvimento<br /> de sistema
+      e ingles pela faculdade UNINTER.
+    </div>
+    </div>
     </SkinCss>
   )
 }
--- src/Home/Styled/SkinCss.js
+++ src/Home/Styled/SkinCss.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 export const SkinCss = styled.div`
 .corpo{background-color: rgba(16, 34, 38, 0.5); 
 width: 1000px;
-height: 870px;
+height: 600px;
 margin-left: 130px;
 border-radius: 15px;
 color: white;
@@ -18,90 +18,56 @@ border-style: dashed solid dashed solid;}
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
 }
-
-.card{
-  width: 70%;
-  height: 150px;
-  background-color:  rgba(16, 34, 38, 1);
-  display: grid;
-  grid-template-columns: 1fr 5fr;
-  border-radius: 12px;
-  box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.25);
-  overflow:hidden;
- margin:10px;
- border: 2px solid rgba(10, 164, 136, 1);
-}
-.fotoCard{
-  display:flex;
-  align-items: center;
-  justify-content: flex-end;
+.card-corpo{
   position: relative;
+  width: 100%;
+  padding: 0 10px;
+  box-sizing: border-box;
 }
-.texto{
-  width: 102%;
-  height: 98%;
-  margin-left: -117px;
-  background-color:  rgba(16, 34, 38, 1);
-  box-shadow: 14px 0px 4px rgba(0, 0, 0, 0.25);
-  z-index: +1 !important;
-  border: 2px solid rgba(10, 164, 136, 1);
-  border-radius: 12px;
-  h3{
-    margin:8px;
-  }
-  p{
-    padding:10px;
-    font-size: 14px;
-    
-  }
+.card-corpo:active, :hover, :focus{
+  outline: none;
+  border: none;
 }
-
-img{
- position: absolute;
-width: 90px;
-margin-left: 590px;
-margin-top:27px;
-border-radius: 10px;
-}
-.bonus{
-  background-color:#ffffffb4;
-  padding: 6px;
-  color: black;
-  width: 300px;
-  margin-left: 25px;
-  border-left: 3px solid yellow;
-  text-align:center;
-}
-@media only screen and (max-width: 990px){
-    .corpo{
-        margin-left: 10px;
-        height: 880px;
+.card{
+  width: 100%;
+  height: 350px;
+  border-radius: 15px;
+  overflow: hidden;
+  cursor: pointer;
+  position: relative;
+  .card-img{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 2;
+    transition: .5s;
+    img{
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
     }
-   .card{
-    height: 150px;
-    width: 95%;
- 
-   }
-   .texto{
-  margin-left: -77px;
-  width: 98%;
-  h3{
-    margin:5px;
+    img:hover{
+      opacity: .4;
+      filter: grayscale(1);
+      transition: .5s;
+    }
   }
-  p{
-    padding:10px;
-    font-size: 12px;
-    margin-top: -10px;
-    
-  }
-  
+  .card-img:hover{
+      transform:translateY(-280px);
+      transition:all .9s;
+    }
 }
-img{
- position: absolute;
-width: 90px;
-margin-left: 353px;
-margin-top:27px;
-border-radius: 10px;
-}
-}
+.detalhes{
+      background-color:rgba(255, 255, 0, 0.100);
+      text-align: center;
+      margin: 35px;
+      color: white;
+      width: 350px;
+      padding: 10px;
+      border-left: 2px solid yellow;
+      border-radius: 5px;
+      
+    }
 `
