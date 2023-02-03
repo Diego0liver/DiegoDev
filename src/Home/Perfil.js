7b30a6506fd75b3bc0e9662a702548a57e2f674e
Diego Junio de Oliveira Fonseca <diegoju95@hotmail.com>
2023-02-03 19:27:02 -0300

    edit text

--- src/Home/Perfil.js
+++ src/Home/Perfil.js
@@ -11,11 +11,11 @@ const Perfil = () => {
       <div className='perfil'>
       <h1>Diego Oliveira Fonseca</h1>
       <h3>Programador web</h3>
-      <p>Programador web front-end Jr autodidata, estudo desenvolvimento web técnologo na Unopar,
+      <p>Programador web autodidata, conclui curso ProgramadorBr fullstack em JavaScript,
+        atualmente estudo urso Técnico em desenvolvimento web na Unopar,
          moro em Maringá Pr, tenho 27 anos de idade, estudo programação a 1 ano, conhecimento e 
-         experiência em projetos próprios com as linguagem JavaScript, React Js, NextJs, 
-         além de ferramentas como o bootstrap, Git, GitHub, API, Figma. Conhecimento basico 
-         em back-end com nodejs, express, sequelize e mysql.
+         experiência em projetos próprios com as linguagem JavaScript, React Js, NextJs, NodeJs.
+         Banco de dados MySQL e também ferramentas como o bootstrap, sass, Git|GitHub, API e Figma.
          Tenho facilidade para aprender linguagens novas também disposição, sou apaixonado por códigos.
          Comunicativo, facilidade trabalha com equipe e criativo. </p>
          <a href='https://drive.google.com/file/d/1qdUk8M8YaRyYFZQDTRjUAfxsKboD1_t3/view?usp=share_link'>
